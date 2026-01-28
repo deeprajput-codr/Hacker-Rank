f03a6c3da0539edc12e80ee0a7da992246eeff79
Deepanshu Rajput <codeindeepc@gmail.com>
2026-01-28 10:00:12 +0530

    Update q5.java

--- q5.java
+++ q5.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
 
 class cricket {
-    String name;
-    String countryName;
-    String skill;
+     private String name;
+     private String country;
+     private String skill;
+    
+   cricket(String input) {
+        String[] parts = input.split(",");
+        name = parts[0].trim();
+        country = parts[1].trim();
+        skill = parts[2].trim();
+    }
     public void printDetail(){
        
         System.out.println("Player Name : " + name);
-        System.out.println("Player Country : " + countryName);
-        System.out.println("Player Skill : " + skill);
+        System.out.println("Country Name :  " + country);
+        System.out.println("Skill :  " + skill);
     }
 }
 public class q5 {
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-        cricket player = new cricket();
+        Scanner sc = new Scanner(System.in);
+        String det = sc.nextLine();
+        cricket player  = new cricket(det);
         System.out.println("Enter the player details");
         System.out.println("Player Details ");
-        player.name = sc.nextLine();
-        player.countryName = sc.nextLine();
-        player.skill = sc.nextLine();
+        
         player.printDetail();
         sc.close();
     }
